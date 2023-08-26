b7346f0d7ca34c1acca791442f768d856d37d21d
Dan Carlton <danlouis.carlton@gmail.com>
2023-08-25 23:08:53 -0700

    commenting on each line of code

--- server.js
+++ server.js
@@ -31,73 +31,113 @@ app.use(express.json())
 
 
 app.get('/',async (request, response)=>{
+    // reads a request on the root rout and passes the request and eventually the response
     // const todoItems = await db.collection('todos').find().toArray()
     // const itemsLeft = await db.collection('todos').countDocuments({completed: false})
     // response.render('index.ejs', { items: todoItems, left: itemsLeft })
     db.collection('todos').find().toArray()
+    // request goes to the todo db collection and finds all everything in the db collection and turns it to an array
     .then(data => {
+    // do something with the data from the db
         db.collection('todos').countDocuments({completed: false})
+        // count the documents within the db and check to see if its completed
         .then(itemsLeft => {
+        // whatever item is left, run a function
             response.render('index.ejs', { items: data, left: itemsLeft })
+            // the response is to render the ejx using the items data
         })
     })
     .catch(error => console.error(error))
+    // if the function catches an error log that error to the console
 })
 
-// create and add a todo item
 app.post('/addTodo', (request, response) => {
+    // create and add the request to create a todo item on the addTodo route and waits for a response
     db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
+    // go to the todos collection in the db and insterts the todoItem
     .then(result => {
+    // a callback function that runs the result of the todo creation if it runs successfully
         console.log('Todo Added')
+        // logs "Todo Added" to the console
         response.redirect('/')
+        // response by redirecting to the route route
     })
     .catch(error => console.error(error))
+    // if theres an error log the error to the console
 })
 
 app.put('/markComplete', (request, response) => {
+    // updates the request being taken in from the markComplete route and waits to respond
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        // go to the todos db collection and update the data
         $set: {
+            // set the data to something
             completed: true
-          }
+            // set completed to true
+        }
     },{
         sort: {_id: -1},
+        // sort the data from top to bottom
         upsert: false
+        // dont upsert the data
     })
     .then(result => {
+        // run a callback if its successfully completed
         console.log('Marked Complete')
+        // log "Marked Complete" to the console
         response.json('Marked Complete')
+        // respond the string in json format
     })
     .catch(error => console.error(error))
+    // if theres an error log the error to the console
 
 })
 
 app.put('/markUnComplete', (request, response) => {
+    // update the markUnComplete route with the request and wait for the response
     db.collection('todos').updateOne({thing: request.body.itemFromJS},{
+        // go to the todos collection in the database and update the body from the data
         $set: {
+            // change the data
             completed: false
-          }
+            // change completed to false
+        }
     },{
         sort: {_id: -1},
+        // sort ascending
         upsert: false
+        // set upsert to false
     })
     .then(result => {
+        // run a callback if its successfully completed
         console.log('Marked Complete')
+        // log "Marked Complete" to the console
         response.json('Marked Complete')
+        // respond the string in json format
     })
     .catch(error => console.error(error))
+    // if theres an error log the error to the console
 
 })
 
 app.delete('/deleteItem', (request, response) => {
+    // delete an item from the request and wait to respond
     db.collection('todos').deleteOne({thing: request.body.itemFromJS})
+    // go to find the item from the todos collection in the db and delete the item matching the requests body
     .then(result => {
+        // run a callback if its successfully completed
         console.log('Todo Deleted')
+        // log "Todo Deleted" to the console
         response.json('Todo Deleted')
+        // respond to the string "Todo Deleted" in json format
     })
     .catch(error => console.error(error))
+    // if theres an error console log the error
 
 })
 
 app.listen(process.env.PORT || PORT, ()=>{
+// listen for the PORT from the .env file or the variable from this file and run a function
     console.log(`Server running on port ${PORT}`)
+    // if successful console.log the template literal string
 })
